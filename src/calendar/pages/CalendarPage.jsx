f1d45c4e7a7c7d4af824a07d5c43b1402120a159
m01x <fortiz.informatica@gmail.com>
2024-11-21 14:49:52 -0300

    feat( Calendar ): Se implemento el calendario desde react-big-calendar y se implemento el idioma español desde el curo.

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,6 +1,55 @@
+import { Calendar } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+import { NavBar } from "../"
+import { addHours } from 'date-fns';
+import { localizer, getMessagesES } from '../../helpers';
+
+
+
+
+const events = [{
+  title: 'Cumpleaños del jefe',
+  notes:'Hay que comprar pastel',
+  start:new Date(),
+  end: addHours( new Date(), 1 ), //Suma una hora...
+  bgColor:'#fafafa',
+  user: {
+    _id:'123',
+    name:'Flavio'
+  }
+}]
+
 
 export const CalendarPage = () => {
+
+  const eventStyleGetter = ( event, start, end, isSelected ) => {
+   console.log(event, start, end, isSelected) ;
+
+   const style = {
+    backgroundColor: '#347CF7',
+    borderRadius: '0px',
+    opacity: 0.8,
+    color: 'white'
+   }
+
+   return {style};
+  }
+
   return (
-    <div>CalendarPage</div>
+    <>
+      <NavBar />
+      <Calendar
+        culture='es'
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc(100vh - 80px)' }}
+        messages={ getMessagesES() }
+        eventPropGetter={ eventStyleGetter } //Esto para empezar a registrar eventos. similar a un onClick
+        
+    />
+    </>
   )
 }
