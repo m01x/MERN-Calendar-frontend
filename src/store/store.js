4f5d8106960723ff348326e3c926e92756ad69f0
m01x <fortiz.informatica@gmail.com>
2024-12-07 13:30:49 -0300

    feat ( login y registro) : se implementa el custom hook de useForm que tengo en mis otros proyectos, para el manejo del formulario del registro y login

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { calendarSlice, uiSlice } from "./";
+import { calendarSlice, uiSlice , authSlice} from "./";
 
 export const store = configureStore({
     reducer: {
         ui: uiSlice.reducer, //Aca quede, minuto 6:11 , instalacion y config de redux
         calendar: calendarSlice.reducer,
+        auth: authSlice.reducer
     },
     middleware: ( getDefaultiddleware ) => getDefaultiddleware({
         serializableCheck: false
